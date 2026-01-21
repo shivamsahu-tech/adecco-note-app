14511789fe58253d9355c083898a662293c04fcd
Shivam Sahu <shivamsahu.tech@gmail.com>
2026-01-21 14:15:54 +0530

    rename api endpoints

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,9 +1,16 @@
+import { cookies } from "next/headers";
+
 export default async function POST(req : Request) {
     try {
         
         const {email, password} = await req.json();
         
-        
+        const cookieStore = await cookies();
+
+        const accessToken = await cookieStore.get("accessToken")
+
+
+
 
 
 
